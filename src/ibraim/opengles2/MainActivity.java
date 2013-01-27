d1ebf48974298fb5cc89aeefddc62dca46e3737b
Rafael Ibraim <ibraim.gm@gmail.com>
2013-01-27 13:42:01 -0200

    Added basic application structure for the samples

--- src/ibraim/opengles2/MainActivity.java
+++ src/ibraim/opengles2/MainActivity.java
@@ -1,15 +1,40 @@
 package ibraim.opengles2;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity
+public class MainActivity extends ListActivity
 {
+  private final String tests[] = {"Epilepsy", "Triangle2d"};
+
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests));
+  }
+
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id)
+  {
+    super.onListItemClick(l, v, position, id);
+    String name = tests[position];
+
+    try
+    {
+      Class c = Class.forName("ibraim.opengles2." + name + "Activity");
+      Intent i = new Intent(this, c);
+      startActivity(i);
+    }
+    catch (ClassNotFoundException e)
+    {
+      e.printStackTrace();
+    }
   }
 }
